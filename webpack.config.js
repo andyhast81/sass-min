d376c3a8ec99ea849a220bfd2422203c455acd32
andyhast81 <andyhast81@gmail.com>
2020-11-22 18:21:47 -0600

    removing unused packages

--- webpack.config.js
+++ webpack.config.js
@@ -1,22 +1,12 @@
 const path = require('path');
-const webpack = require('webpack');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
-const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
-const plugins = [
-  new MiniCssExtractPlugin({
-    // Options similar to the same options in webpackOptions.output
-    // both options are optional
-    filename: '[name].css',
-    chunkFilename: '[id].css',
-  }),
-];
+
 module.exports = {
   entry: './src/ti-index.js',
   output: {
     filename: 'ti-index.js',
     path: path.resolve(__dirname, 'assets'),
   },
-  plugins,
+  // plugins,
     module:{
         rules: [
             {
